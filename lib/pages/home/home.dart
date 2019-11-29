35e91f471d204d9a7e31b8a6e2b90e763bca8602
NecronizerFTW <varun.h.khatri@gmail.com>
2019-11-29 16:52:42 +0530

    QR Code Setup

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:ec_senior/services/auth.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatelessWidget {
   // AuthService instance.
-  AuthService _authService = AuthService();
+  final AuthService _authService = AuthService();
+
   // Build Method.
   @override
   Widget build(BuildContext context) {
+
+    // Getting the current Firebase user from Provider Stream.
+    FirebaseUser _currentFirebaseUser = Provider.of<FirebaseUser>(context);
+
+    // Build the Bottom Sheet with the required QR Code.
+    void _buildQrCodeBottomSheet () {
+      showModalBottomSheet(context: context, builder: (context) {
+        return Center(
+          child: QrImage(
+            data: _currentFirebaseUser.uid,
+            version: QrVersions.auto,
+            size: 250.0,
+          ),
+        );
+      });
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -19,6 +40,10 @@ class Home extends StatelessWidget {
                 return await _authService.signOut();
               },
               child: Text('Log out'),
+            ),
+            FlatButton(
+              onPressed: _buildQrCodeBottomSheet,
+              child: Text('Generate QR Code'),
             )
           ],
         ),
