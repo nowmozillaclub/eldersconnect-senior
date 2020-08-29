ee83b29931dc50acc126ce77073dd0569865e2a6
Aditi-Mohan <54301942+Aditi-Mohan@users.noreply.github.com>
2020-08-29 18:53:05 +0530

    Established channel between native and flutter

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,6 +1,7 @@
 import 'package:ec_senior/commons/bottom_nav_bar.dart';
-import 'package:ec_senior/utils/text_styles.dart';
+//import 'package:ec_senior/utils/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SecondPage extends StatefulWidget {
   @override
@@ -8,8 +9,30 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  static final platform = MethodChannel('ec_senior/alarm_service');
+
+  Future<void> response(MethodCall call) async {
+    switch(call.method) {
+      case 'response':
+        hiFromFlutter(call.arguments);
+        break;
+      default:
+        print('no corresponding method');
+    }
+  }
+
+  void hiFromFlutter(dynamic args) {
+    print('Hi ${args.toString()}');
+  }
+
+  static final String CHANNEL_ID = 'RESPONSE_CHANNEL';
+
   @override
   Widget build(BuildContext context) {
+
+    MethodChannel flutterChannel = MethodChannel(CHANNEL_ID);
+    flutterChannel.setMethodCallHandler(response);
+
     return WillPopScope(
       onWillPop: () async {
         setState(() {
@@ -19,7 +42,17 @@ class _SecondPageState extends State<SecondPage> {
       },
       child: Scaffold(
         body: Center(
-          child: Text('Second Page', style: MyTextStyles.heading,),
+          child: FlatButton(
+            child: Text('Call Native'),
+            onPressed: () async {
+              try {
+                var res = await platform.invokeMethod('helloFromNativeCode');
+                print(res);
+              } on PlatformException catch(e) {
+                print('failed, error: $e');
+              }
+            },
+          )
         ),
         bottomNavigationBar: BottomNavBar(currentSelected: currentSelectedNavBar),
       ),
